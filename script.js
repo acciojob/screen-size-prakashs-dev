d8756e6814a4022a23924665c32ccde6bce17498
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-28 20:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const header = document.getElementById("sizeInfo");
+window.addEventListener("resize", () => {
+	header.textContent = `Width: ${window.outerWidth} and Height: ${window.outerHeight}`;
+});
